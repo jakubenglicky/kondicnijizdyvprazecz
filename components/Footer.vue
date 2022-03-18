192e8b1dfc66a47c9615081cd30af68692c462de
vydrova <v.janca@gmail.com>
2022-03-18 16:10:02 +0100

    Footer a tak obecně

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,3 +1,12 @@
 <template>
-    
+    <footer>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4">
+                    Autoškola<br>
+                    Praha sjdhd
+                </div>
+            </div>
+        </div>
+    </footer>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -23,25 +23,54 @@
       </div>
     </section>
 
+    <section id="procvicite">
+      <div class="container u-center">
+        <h2 data-aos="fade-right" data-aos-delay="200">Díky kondičním jízdám například procvičíte</h2>
+        <div class="pro-box-row">
+          <div class="row">
+            <div class="col-6 col-md-3 pro-box">
+              <div data-aos="zoom-in" data-aos-delay="200" class="pro-box__ikon">
+                <font-awesome-icon icon="fa-solid fa-traffic-light" />  
+              </div>
+              <div data-aos="zoom-in" data-aos-delay="200">Jízdu v hustém městském provozu a na složitých křižovatkách.</div>          
+            </div>
+            <div class="col-6 col-md-3 pro-box">
+              <div data-aos="zoom-in" data-aos-delay="200" class="pro-box__ikon">
+                <font-awesome-icon icon="fa-solid fa-mountain" />
+              </div>
+              <div data-aos="zoom-in" data-aos-delay="200">Rozjíždění do kopce.</div>
+            </div>
+            <div class="col-6 col-md-3 pro-box">
+              <div data-aos="zoom-in" data-aos-delay="200" class="pro-box__ikon">
+                <font-awesome-icon icon="fa-solid fa-square-parking" />
+              </div>
+              <div data-aos="zoom-in" data-aos-delay="200">Kolmé i rovnoběžné parkování všemi směry a couvání.</div>
+            </div>
+            <div class="col-6 col-md-3 pro-box">
+              <div data-aos="zoom-in" data-aos-delay="200" class="pro-box__ikon">
+                <font-awesome-icon icon="fa-solid fa-snowflake" />
+              </div>
+              <div data-aos="zoom-in" data-aos-delay="200" class="">Jízdu na sněhu, za mokra nebo na náledí.</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
     <section id="pro-koho">
       <div class="container">
-        <h2 class="u-center">Díky kondičním jízdám například procvičíte</h2>
         <div class="row">
-          <div class="col-md-3 pro-box">
-            <font-awesome-icon icon="fa-solid fa-traffic-light" />  
-            <div>Jízdu v hustém městském provozu a na složitých křižovatkách.</div>          
-          </div>
-          <div class="col-md-3 pro-box">
-            <font-awesome-icon icon="fa-solid fa-mountain" />
-            <div>Rozjíždění do kopce.</div>
-          </div>
-          <div class="col-md-3 pro-box">
-            <font-awesome-icon icon="fa-solid fa-square-parking" />
-            <div>Kolmé i rovnoběžné parkování všemi směry a couvání.</div>
-          </div>
-          <div class="col-md-3 pro-box">
-            <font-awesome-icon icon="fa-solid fa-snowflake" />
-            <div class="">Jízdu na sněhu, za mokra nebo na náledí.</div>
+          <div class="col-md-8 u-m-auto u-center">
+            <h2 data-aos="fade-roght" data-aos-delay="200">Pro koho jsou kondiční jízdy vhodné</h2>
+            <div  data-aos="fade-up" data-aos-delay="200">
+              <ul>
+                <li>Pro zájemce, kteří neřídí pravidelně, ale chtějí si uchovat řidičské dovednosti.</li>
+                <li>Pro ty, kteří se chtějí ještě zdokonalit v řízení, například čerstvě po získání řidičského průkazu.</li>
+                <li>Kondiční jízdy lze využít ale i pro průjezd konkrétní trasou města, kde je například hustý provoz, nebo náročné křižovatky apod.</li>
+                <li>Prostě pro všechny, kteří chtějí získat více jistoty v řízení svého vozidla.</li>
+              </ul>
+              <p>Jízdy provádíme individuálně po domluvě se zájemcem, v rozsahu odpovídajícím jeho schopnostem. Maximální počet kondičních jízd není stanoven.</p>
+            </div>
           </div>
         </div>
       </div>
